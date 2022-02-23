31b94a318eaf2c84a1c86df6a0a3eb102af8a8cf
Harikrishnan R <hari04.krishnan@gmail.com>
2022-02-23 17:59:31 +0530

    Implement dependency injection

--- src/main/java/com/upgrad/greeting/App.java
+++ src/main/java/com/upgrad/greeting/App.java
@@ -6,8 +6,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.upgrad.greeting");
-        GreetingService greetingService = (GreetingService) context.getBean("englishGreetingService");
+        EnglishGreetingService greetingService = (EnglishGreetingService) context.getBean("englishGreetingService");
         greetingService.greet("Hari");
+        System.out.println(greetingService.getTimeService().getCurrentTime());
 
     }
 }
--- src/main/java/com/upgrad/greeting/EnglishGreetingService.java
+++ src/main/java/com/upgrad/greeting/EnglishGreetingService.java
@@ -1,9 +1,21 @@
 package com.upgrad.greeting;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EnglishGreetingService implements GreetingService{
+    @Autowired
+    private TimeService timeService;
+
+    public TimeService getTimeService() {
+        return timeService;
+    }
+
+    public void setTimeService(TimeService timeService) {
+        this.timeService = timeService;
+    }
+
 
     @Override
     public void greet(String name) {
