b53312ab890dc695232fb6986fda88f45295f577
Harikrishnan R <hari04.krishnan@gmail.com>
2022-02-23 19:00:17 +0530

    Bean scope

--- src/main/java/com/upgrad/greeting/App.java
+++ src/main/java/com/upgrad/greeting/App.java
@@ -6,9 +6,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.upgrad.greeting");
-        EnglishGreetingService greetingService = (EnglishGreetingService) context.getBean("englishGreetingService");
-        greetingService.greet("Hari");
-        System.out.println(greetingService.getTimeService().getCurrentTime());
+        EnglishGreetingService greetingService1 = (EnglishGreetingService) context.getBean("englishGreetingService");
+        EnglishGreetingService greetingService2 = (EnglishGreetingService) context.getBean("englishGreetingService");
 
+        System.out.println((greetingService2 == greetingService1));
     }
 }
--- src/main/java/com/upgrad/greeting/EnglishGreetingService.java
+++ src/main/java/com/upgrad/greeting/EnglishGreetingService.java
@@ -2,9 +2,13 @@ package com.upgrad.greeting;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+
 public class EnglishGreetingService implements GreetingService{
 
     private TimeService timeService;
